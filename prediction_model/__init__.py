7d700096062b50a019d592db6456162a9b451fba
Sivabalan <sivansbhfe@gmail.com>
2025-05-10 09:05:54 +0530

    18 push

--- prediction_model/__init__.py
+++ prediction_model/__init__.py
@@ -1,4 +1,4 @@
 import os
 from prediction_model.config import config
-with open(os.path.join(config.PACKAGE_ROOT,'version')) as f:
+with open(os.path.join(config.PACKAGE_ROOT,'VERSION')) as f:
     __version__=f.read().strip()
